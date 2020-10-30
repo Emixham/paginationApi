b2d8e790fb82d3f3ee897adcaff5a13e50bf551d
Guiroy Kenny <kenny.guiroy@epsi.fr>
2020-10-30 15:24:00 +0100

    Class + Package Model

---
+++ 1/src/main/java/model/Produit.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Produit {
+	
+	private Long id;
+	private String nom;
+	private Float prix;
+	private String SKU;
+	private String description;
+	private String options;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Float getPrix() {
+		return prix;
+	}
+	public void setPrix(Float prix) {
+		this.prix = prix;
+	}
+	public String getSKU() {
+		return SKU;
+	}
+	public void setSKU(String sKU) {
+		SKU = sKU;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getOptions() {
+		return options;
+	}
+	public void setOptions(String options) {
+		this.options = options;
+	}
+	
+	
+
+}
