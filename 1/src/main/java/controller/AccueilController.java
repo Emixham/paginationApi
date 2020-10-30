f6774eecc9a8a6335839ae9ddf472caf2bd3f992
Emixham <maximeD.ipod@gmail.com>
2020-10-30 15:38:33 +0100

    service

--- 1/src/main/java/controller/AccueilController.java
+++ 1/src/main/java/controller/AccueilController.java
@@ -1,5 +1,14 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AccueilController {
 
+	@GetMapping("/")
+	public String accueil() {
+		return "accueil";
+	}
+	
 }
